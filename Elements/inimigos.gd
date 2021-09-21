72ff1c4f13e5bc63c4c8151b75bb79e16ef08555
raktanaka <raktanaka@gmail.com>
2021-09-21 14:43:44 -0300

    Arrumado algumas referências a scripts e escala do sprite

--- Elements/inimigos.gd
+++ Elements/inimigos.gd
@@ -1,13 +1,28 @@
 extends KinematicBody2D
 
+var speed = 0.75
+var move_direction = 0
+onready var path_follow = get_parent()
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	MovementLoop(delta)
+
+func MovementLoop(delta):
+	var pos_ini = path_follow.get_global_position()
+	path_follow.set_offset(path_follow.get_offset() + speed + delta)
+	var pos = path_follow.get_global_position()
+	move_direction = (pos.angle_to_point(pos_ini) / 3.14) * 100
+	
