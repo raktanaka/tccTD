44d0679b145b6db3198a9f86ee4be819620d10b2
RGPRafael <rafaelgpdasilva@gmail.com>
2021-09-27 19:45:55 -0300

    adicionando botoes para escolher torres, assim que adicionadas ja podem ser utilizadas olhar tutorial ?

--- Elements/torres.gd
+++ Elements/torres.gd
@@ -8,11 +8,11 @@ func olhe_para():
 	pass
 
 func disparo(delta):
-	var tiro = preload("res://Elements/Disparo.tscn").instance()
-	var torres = get_tree().get_root().get_children()
-	print(torres)
-	print(torres[0]) 
-	print(torres[0].get_name()) 
+#	var tiro = preload("res://Elements/Disparo.tscn").instance()
+#	var torres = get_tree().get_root().get_children()
+#	print(torres)
+#	print(torres[0]) 
+#	print(torres[0].get_name()) 
 	pass
 
 
@@ -21,10 +21,11 @@ func _on_ViewRadius_body_entered(body):
 
 func _physics_process(delta):
 	olhe_para()
-	if Input.is_action_pressed("atirar"): 
-		yield(get_tree().create_timer(0.09), "timeout")
-		if Input.is_action_just_released("atirar"):
-			print('CLICK')
-			disparo(delta)
+	
+#	if Input.is_action_pressed("atirar"): 
+#		yield(get_tree().create_timer(0.09), "timeout")
+#		if Input.is_action_just_released("atirar"):
+#			print('CLICK')
+#			disparo(delta)
 	pass
 	
