b9899d00322c18e39045ac4d183c4e83d9d60007
RGPRafael <rafaelgpdasilva@gmail.com>
2021-10-01 18:29:23 -0300

    melhorando os botoes de torres e fazendo inimigos andarem no caminho..segundo o tutorial do discord

--- Singletons/GameData.gd
+++ Singletons/GameData.gd
@@ -1,11 +1,14 @@
 extends Node
 
+var jogo_comecou = false
+
+
 var tower_data = {
-	"torreverde": {
+	"TowerGreen": {
 		"damage": 20,
 		"rof": 1, #rate of fire
 		"range": 350},
-	"torrevermelha": {
+	"TowerRed": {
 		"damage": 10,
 		"rof": 1, #rate of fire
 		"range": 550},
