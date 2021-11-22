39206117b120b2cc60ac15887901bab2524d06ec
raktanaka <raktanaka@gmail.com>
2021-11-22 20:11:41 -0300

    Set up for testing mode loading scenarios, with receiving parameters

--- Elements/TestMode.gd
+++ Elements/TestMode.gd
@@ -5,13 +5,17 @@ func _ready():
 	load_test_menu()
 	
 func load_test_menu():
-	get_node("TestMenu/buttons/TestMode").connect("pressed",self,"start_test")
-	get_node("TestMenu/buttons/Return").connect("pressed",self,"return_main")
+	$TestMenu/buttons/TestMode.connect("pressed",self,"start_test")
+	$TestMenu/buttons/Return.connect("pressed",self,"return_main")
+
 	
 func start_test():
-	pass
-	get_node("Menu").queue_free()
-	var scene = load('res://Elements/MainScene.tscn').instance()
+	var params = get_params()
+	var towers = params[0]
+	var enemies = params[1]
+	$TestMenu.queue_free()
+	var scene = load('res://Elements/TestScene.tscn').instance()
+	scene.init_params(towers, enemies)
 	scene.connect("game_finished",self, 'unload_game')
 	add_child(scene)
 
@@ -20,4 +24,17 @@ func return_main():
 	var main_menu = load('res://Elements/GerenciarCena.tscn').instance()
 	add_child(main_menu)
 	
-	
+func get_params():
+	var towers
+	var enemies
+	if $TestMenu/towers/AllGreen.get_button_group().get_pressed_button() == null:
+		pass
+	else:
+		towers = $TestMenu/towers/AllGreen.get_button_group().get_pressed_button().get_name()
+	if $TestMenu/enemies/AI.get_button_group().get_pressed_button() == null:
+		pass
+	else:
+		enemies = $TestMenu/enemies/AI.get_button_group().get_pressed_button().get_name()
+		
+	return [towers, enemies]
+
